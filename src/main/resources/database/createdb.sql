e1518cf167f7a170b4b1d728fb6fce5152059787
acalbaza <acalbaza@ARTSOFT116.artsoft.ro>
2016-05-20 15:10:51 +0300

    test

--- src/main/resources/database/createdb.sql
+++ src/main/resources/database/createdb.sql
@@ -1,10 +1,2 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- * Author:  acalbaza
- * Created: May 20, 2016
- */
-
+CREATE USER smirna WITH PASSWORD 'smirna';
+CREATE DATABASE demo OWNER smirna;
